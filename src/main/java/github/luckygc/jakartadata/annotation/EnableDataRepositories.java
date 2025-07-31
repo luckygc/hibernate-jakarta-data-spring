e1a6b488234c8bebfda3483b32a3f6aa562f0fcc
luckygc <chenwwecm@gmail.com>
2025-07-31 22:16:08 +0800

    修复checkstyle问题

--- src/main/java/github/luckygc/jakartadata/annotation/EnableDataRepositories.java
+++ src/main/java/github/luckygc/jakartadata/annotation/EnableDataRepositories.java
@@ -3,7 +3,11 @@ package github.luckygc.jakartadata.annotation;
 import github.luckygc.jakartadata.DataRepositoryRegistrar;
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * 启用 Jakarta Data Repository 支持的注解
